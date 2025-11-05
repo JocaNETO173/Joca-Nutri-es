fe531058d12c3296580372f2cd5e1e75a90fafe2
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2025-11-05 09:48:44 -0300

    Add files via upload

--- js/remove-paciente.js
+++ js/remove-paciente.js
@@ -1,5 +1,16 @@
-pacientes.forEach(function(paciente){
+const tabelaCorpo = document.querySelector('tbody');
+
+tabelaCorpo.addEventListener('dblclick', function(event){
+    event.target.parentNode.classList.add('fadeOut');
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+    //event.target.parentNode.remove();
+    
+});
+
+/*pacientes.forEach(function(paciente){
     paciente.addEventListener('dblclick', function(){
         this.remove();
     });
-});
+}); */
